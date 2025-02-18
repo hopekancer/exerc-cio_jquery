aeeaa424c95509bbaf11e7e7e2c5a21345999485
Hugo Casarisi <66367598+hopekancer@users.noreply.github.com>
2025-02-18 09:21:44 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+let inputTarefaValue = ``
+const lista = $(`#lista`)
+const novoItem = $(`<li style="display: none;"></li>`)
+
+    $(`form`).on(`submit`, function(e) {
+        e.preventDefault()
+        inputTarefaValue = $(`#input-tarefa`).val()
+        $(`<li>
+            <a href="#">${inputTarefaValue}</a>
+        </li>`).appendTo(lista)
+        $(`#input-tarefa`).val(``)
+    })
+
+    lista.on('click', 'li a', function(e) {
+        e.preventDefault(); // Evita que o link redirecione
+        $(`li a`).toggleClass('riscado'); // Alterna a classe 'riscado'
+    });
+    
+})
